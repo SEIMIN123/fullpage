fced39001e2afd7d65c22070f3a4a14e13182436
unknown <interkp12@gmail.com>
2022-08-02 17:37:06 +0900

    .

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(function(){
 
 	$('#fullpage').fullpage({
 		//options here
-    anchors:['section1','section2','section3','section4','section5']
+    anchors:['section1','section2','section3','section4','section5'],
+    sectionsColor:['#ccc', '#bbb', '#fff', '#bea', '#c2c3ff']
 	});
 
 });
